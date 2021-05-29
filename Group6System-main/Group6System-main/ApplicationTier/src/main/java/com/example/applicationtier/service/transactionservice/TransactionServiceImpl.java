05b7a1db1e384692fc624a53f2a2c29dfff23742
Priyanka Shrestha <74237122+PriyankaShrestha@users.noreply.github.com>
2021-05-30 01:26:41 +0200

    Transaction history completed

--- Group6System-main/Group6System-main/ApplicationTier/src/main/java/com/example/applicationtier/service/transactionservice/TransactionServiceImpl.java
+++ Group6System-main/Group6System-main/ApplicationTier/src/main/java/com/example/applicationtier/service/transactionservice/TransactionServiceImpl.java
@@ -2,13 +2,16 @@ package com.example.applicationtier.service.transactionservice;
 
 import com.example.applicationtier.DAO.transaction.TransactionDAO;
 import com.example.applicationtier.models.Account;
+import com.example.applicationtier.models.Notification;
 import com.example.applicationtier.models.Transaction;
+import com.example.applicationtier.service.notification.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionServiceImpl implements TransactionService{
     @Autowired private TransactionDAO transDAO;
+    @Autowired private NotificationService notificationService;
 
     @Override
     public String transferMoney(Transaction transaction) throws Exception{
@@ -29,16 +32,21 @@ public class TransactionServiceImpl implements TransactionService{
         }
     }
 
-    private void updateReceiver(Transaction transaction) {
+    private void updateReceiver(Transaction transaction) throws Exception {
         double getReceiverBalance = transaction.getReceiver().getBalance();
         double newReceiverBalance = getReceiverBalance + transaction.getAmount();
         transaction.getReceiver().setBalance(newReceiverBalance);
+        notificationService.sendNotificationToUser(transaction.getReceiver().getUser().getUsername(), transaction.getAmount() + "kr received from " +
+                transaction.getSender().getUser().getUsername() + " Message: " + transaction.getMessage());
     }
 
-    private void updateSender(Transaction transaction) {
+    private void updateSender(Transaction transaction) throws Exception {
         double getSenderBalance = transaction.getSender().getBalance();
         double newSenderBalance = getSenderBalance - transaction.getAmount();
         transaction.getSender().setBalance(newSenderBalance);
+        notificationService.sendNotificationToUser(transaction.getSender().getUser().getUsername(), transaction.getAmount() + "kr transfered to " +
+                transaction.getReceiver().getUser().getUsername() + " Message: " + transaction.getMessage());
+
     }
 
     public Account getAccount(String username) throws Exception {
